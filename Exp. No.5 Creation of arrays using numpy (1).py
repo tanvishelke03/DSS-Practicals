2fb3d559cb9c8e6b47516da752becb82ae72b246
Tanvi Shelke <tanvibshelke@gmail.com>
2025-11-09 19:48:55 +0530

    Add files via upload

---
+++ Exp. No.5 Creation of arrays using numpy (1).py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Exp.NO.5
+
+
+# In[2]:
+
+
+#aim:Creation of 1D,2D and multidimentional arrays using numpy
+
+
+# In[5]:
+
+
+#name: Tanvi Shelke
+#roll no: 58
+#subject:Dss
+#batch:b2
+
+
+# In[6]:
+
+
+#importing the basic library
+import numpy as np
+
+
+# In[7]:
+
+
+# creating 1D Array using numpy
+
+
+# In[8]:
+
+
+a1=np.array([10,20,30,40,50])
+
+
+# a1 
+
+# In[9]:
+
+
+a1
+
+
+# In[10]:
+
+
+a2=np.array([[10,20,30,40],[50,60,70,80]])
+
+
+# In[11]:
+
+
+a2
+
+
+# In[12]:
+
+
+#creating 2D array using numpy 
+
+
+# In[13]:
+
+
+a3=np.matrix('10 20;30 40;50 60')
+
+
+# In[14]:
+
+
+a3
+
+
+# In[15]:
+
+
+#creating array of 10 random integers  Between 10 t0 15
+
+
+# In[16]:
+
+
+arr=np.random.randint(10,15,size=10)
+
+
+# In[17]:
+
+
+print(arr)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
